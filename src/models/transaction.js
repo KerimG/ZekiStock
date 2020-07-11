cb9810392e0d6cf1d288c9bdb2890d11d308d148
Kerim Güney <kerim@gueney.io>
2020-07-11 14:06:51 +0200

    initial models

--- src/models/transaction.js
+++ src/models/transaction.js
@@ -1,28 +1,28 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class TransactionId extends Model {
+  class Transaction extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here]
-      this.hasMany(models.Transaction, {
+      // define association here
+      this.belongsTo(models.TransactionId, {
         foreignKey: 'transactionId',
         onDelete: 'CASCADE',
       });
     }
   }
-  TransactionId.init(
+  Transaction.init(
     {
       transctionId: DataTypes.STRING,
     },
     {
       sequelize,
-      modelName: 'TransactionId',
+      modelName: 'Transaction',
     }
   );
-  return TransactionId;
+  return Transaction;
 };
