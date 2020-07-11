448a1284479290a408a2a5335fc59a47f1e051e9
Kerim Güney <kerim@gueney.io>
2020-07-11 13:49:53 +0200

    sequelize init

---
+++ src/config/sequelize.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    storage: __dirname + '../db/dev.db.sqlite',
+    dialect: 'sqlite',
+    logging: console.log,
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
