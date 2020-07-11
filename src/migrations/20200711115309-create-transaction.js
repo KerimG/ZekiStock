f4f3a0024d565b61499f3cbe384eeed0808cadbc
Kerim Güney <kerim@gueney.io>
2020-07-11 14:06:43 +0200

    initial migrations

---
+++ src/migrations/20200711115309-create-transaction.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      transctionId: {
+        type: Sequelize.STRING,
+        eferences: {
+          model: 'TransactionId', // name of Target model
+          key: 'id', // key in Target model that we're referencing
+        },
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Transactions');
+  },
+};
