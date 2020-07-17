8bd6e6c735af93337a53acdc7f9419c4c5126d5b
Kerim Güney <kerim@gueney.io>
2020-07-17 10:31:12 +0200

    add model for transactions

--- src/migrations/20200711115309-create-transaction.js
+++ src/migrations/20200711115309-create-transaction.js
@@ -16,6 +16,67 @@ module.exports = {
         },
         onDelete: 'CASCADE',
       },
+      date: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      product: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      isin: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      market: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      amount: {
+        allowNull: false,
+        type: Sequelize.NUMBER,
+      },
+      quoteCurrency: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      quote: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      localCurrency: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      quoteInLocalCurrsency: {
+        allowNull: false,
+        type: Sequelize.NUMBER,
+      },
+      valueCurrency: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.NUMBER,
+      },
+      fx: {
+        allowNull: false,
+        type: Sequelize.NUMBER,
+      },
+      feeCurrency: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      fee: {
+        allowNull: false,
+        type: Sequelize.NUMBER,
+      },
+      totalCurrency: { allowNull: false, type: Sequelize.STRING },
+      total: {
+        allowNull: false,
+        type: Sequelize.NUMBER,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
